95137c6e89f2b3ad4d40b2585824160f87c6ed09
Danny Pilbeam <Danny Pilbeam>
2023-04-10 06:49:47 +0100

    updated controls

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const ctx0 = canvas0.getContext('2d');
 const ctx1 = canvas1.getContext('2d');
 const ctx2 = canvas2.getContext('2d');
 const ctx3 = canvas3.getContext('2d');
-const width = window.innerWidth *0.8;
+const width = window.innerWidth * 0.8;
 const height = window.innerHeight * 0.8;
+
+
 // Set canvas size
 canvas0.width = width;
 canvas1.width = width;
@@ -20,7 +22,7 @@ canvas2.height = height;
 canvas3.height = height;
 // Set planet class size (s), main color lightness (l) for gradient and secondary lightness (l2) for gradient
 let s = 100;
-let l = 15;
+let l = 25;
 let l2 = l + 40;
 // Debugging
 let results = [];
@@ -49,9 +51,9 @@ class ShootingStar {
         this.draw(ctx1, this.x, this.y);
         this.x += this.vx;
         this.y += this.vy;
-        if(this.x  < canvas0.width+100){
+        if (this.x < canvas0.width + 100) {
             results.push(Math.floor(this.x))
-       
+
         }
         console.table(results)
         requestAnimationFrame(this.animate.bind(this))
@@ -64,21 +66,21 @@ class Planet {
         this.canvas = canvas2;
         this.x = this.canvas.width / 2;
         this.y = this.canvas.height / 2;
-        this.r = 150;
+       
     }
-    draw(context, h) {
+    draw(context, s, h) {
         context.beginPath();
         // Draw atmosphere circle    
-        context.arc(this.x, this.y, this.r * 1.05, 0, 2 * Math.PI);
+        context.arc(this.x, this.y, s * 1.05, 0, 2 * Math.PI);
         context.fillStyle = `hsla(0,0%,100%,0.05)`;
         context.fill();
         context.closePath();
         // Draw planet circle with gradient fill to give "3d" affect
         context.beginPath();
-        const grd = context.createRadialGradient(this.x, this.y, this.r, this.x / 2, this.y / 2, this.r / 5);
+        const grd = context.createRadialGradient(this.x, this.y, s, this.x / 2, this.y / 2, s / 5);
         grd.addColorStop(0, `hsl(${h},${s}%,${l}%)`);
         grd.addColorStop(1, `hsl(${h},${s}%,${l2}%)`);
-        context.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        context.arc(this.x, this.y, s, 0, 2 * Math.PI);
         context.fillStyle = grd;
         context.fill();
         context.closePath();
@@ -121,11 +123,32 @@ for (let i = 0; i < 600; i++) {
     star.draw(ctx0);
 }
 
+let PlanetValues = {
+    size: Math.floor(Math.random() * 200),
+    colour: Math.floor(Math.random() * 360)
+}
 
 
+function setValue() {
+    document.getElementById('sldSize').max = canvas2.width *0.45;
+    const sizeValue = document.getElementById('sldSize').value;
+    const colourValue = document.getElementById('sldColour').value;
+    PlanetValues = {
+        size: sizeValue,
+        colour: colourValue
+    }
+    console.log(PlanetValues);
+    drawPlanet()
+}
+
+function drawPlanet(){
+ctx2.clearRect(0, 0, width, height)
 const planet = new Planet(canvas2);
-let hue = Math.floor(Math.random() * 360);
-planet.draw(ctx2, hue);
+// let hue = Math.floor(Math.random() * 360);
+planet.draw(ctx2 , PlanetValues.size, PlanetValues.colour);
+}
+
+drawPlanet()
 
 // function randomTimer() {
 //     const shootingStar = new ShootingStar(canvas1, 0, 0, 2, 3, 2);
